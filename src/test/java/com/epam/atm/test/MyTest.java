0010a5024378002fe3c78864a8247c8415703ef8
Serhii Lukash <Serhii_Lukash@epam.com>
2023-04-04 14:13:32 +0200

    HW junit

---
+++ src/test/java/com/epam/atm/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.epam.atm.test;
+
+import com.epam.tat.module4.Calculator;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    public void test() {
+        Calculator calculator= new Calculator();
+        calculator.div(2,2) ;
+
+    }
+
+}
